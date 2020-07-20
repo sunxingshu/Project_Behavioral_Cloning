d41d568e4cbfff5796d9695001bb26d7064f7d5f
Xingshu Sun <xingshusun@Xingshus-MacBook-Pro-2.local>
2020-07-20 01:07:08 -0700

    delete unnecessary files

--- train_model_1.py
+++
@@ -1,9 +0,0 @@
-import csv
-
-lines = []
-
-with open('./Sample_Data/driving_log.csv') as csvfile:
-    temp = csv.reader(csvfile)
-    for line in temp:
-        print(line)
-        lines.append(line)
